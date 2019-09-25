ff1ff54883c24be4512eb2957c6c912e2927d623
Gabriel <gvasquez@belatrixsf.com>
2019-09-25 14:14:43 -0300

    testing shell batch folder1

--- .circleci/check_commit_in_folder.sh
+++ .circleci/check_commit_in_folder.sh
@@ -4,7 +4,7 @@ echo "Folder to check: $1"
 LATEST_COMMIT=$(git rev-parse HEAD)
 FOLDER_COMMIT=$(git log -1 --format=format:%H --full-diff $1)
 
-echo "LATEST_COMMIT: "$LATEST_COMMIT
+echo "LATEST_COMMIT: " $LATEST_COMMIT
 echo "FOLDER_COMMIT: " $FOLDER_COMMIT
 
 if [ "$FOLDER_COMMIT" = "$LATEST_COMMIT" ];
