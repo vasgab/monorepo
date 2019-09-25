42c42750d4fe8cb3dd52e02b698a10b7f446843e
Gabriel <gvasquez@belatrixsf.com>
2019-09-25 10:53:08 -0300

    testing shell batch

--- .circleci/check_commit_in_folder.sh
+++ .circleci/check_commit_in_folder.sh
@@ -3,9 +3,7 @@ set -e
 echo "Folder to check: $1"
 
 LATEST_COMMIT=$(git rev-parse HEAD)
-FOLDER1_COMMIT=$(git log -1 --format=format:%H --full-diff Folder-1)
-FOLDER2_COMMIT=$(git log -1 --format=format:%H --full-diff Folder-2)
+FOLDER_COMMIT=$(git log -1 --format=format:%H --full-diff $1)
 
 echo "LATEST_COMMIT: "$LATEST_COMMIT
-echo "FOLDER1_COMMIT: " $FOLDER1_COMMIT
-echo "FOLDER2_COMMIT " $FOLDER2_COMMIT
+echo "FOLDER_COMMIT: " $FOLDER_COMMIT
